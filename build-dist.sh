322864f81a00f7bcf36e0273140b450a5c989001
Scott Stirling <scottstirling@gmail.com>
2026-05-28 16:02:21 -0400

    Add build-dist.sh to automate release and distribution processes partially.  Generated a new signature file for pi2llm-main.js and keeping it in etc/.  It is only needed when packaging and deploying official release distributions.  Update updates.xri for latest release.

--- build-dist.sh
+++ build-dist.sh
@@ -8,7 +8,7 @@ TGZ_FILENAME="${DATE}-pi2llm.tar.gz"
 mkdir -vp $BUILD_DIR/doc/scripts/$NAME
 mkdir -vp $BUILD_DIR/src/scripts/$NAME
 
-cp -v pi2llm-main.js pi2llm-main.xsgn AGENTS.md README.md LICENSE $BUILD_DIR/src/scripts/$NAME/
+cp -v pi2llm-main.js etc/pi2llm-main.xsgn AGENTS.md README.md LICENSE $BUILD_DIR/src/scripts/$NAME/
 cp -rv lib $BUILD_DIR/src/scripts/$NAME/
 cp -rv doc/scripts/$NAME/*.html $BUILD_DIR/doc/scripts/$NAME/
 
